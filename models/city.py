f1f430bfc908b67e2aa47da7142757b35ba8ea90
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2022-10-31 02:40:47 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
-
+"""
+City class inherits from BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Class for managing city objects"""
-    
-    state_id = ""
     name = ""
+    state_id = ""
