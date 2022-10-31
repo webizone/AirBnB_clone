4ae05919a0e319d10c7e616a76949d6f201bf1e4
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2022-10-31 02:44:42 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
+"""
+Amenity class inherits from BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Class for managing user objects"""
-    
     email = ""
     password = ""
     first_name = ""
