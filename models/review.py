316b0500f543333b61bbce11766d1b1786a74350
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2022-10-31 02:42:53 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This module creates a Review class"""
 
+"""
+Amenity class inherits from BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Class for managing review objects"""
-    
     place_id = ""
     user_id = ""
     text = ""
