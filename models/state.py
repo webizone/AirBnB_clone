19420cf2d8d4abe50cedf62288a7d5529dbebb2b
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2022-10-31 02:43:49 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
+"""
+Amenity class inherits from BaseModel
+"""
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    """Class for managing user objects"""
-    
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class State(BaseModel):
+    name = ""
